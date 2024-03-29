f27d081ce5049826e01948dfc3ee302c136d9f2a
tasmaishelke <tasmaishelke@gmail.com>
2024-03-29 11:52:58 +0530

    added locatioon input

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -8,6 +8,8 @@ function Registration()
     const [birthPlace, setBirthPlace] = useState('')
     const [gender, setGender] = useState('')
     const [birthday, setBirthday] = useState('')
+    const [location, setLocation] = useState('')
+
     
     const details = (e) =>
     {
@@ -16,6 +18,8 @@ function Registration()
         console.log(birthPlace)
         console.log(gender)
         console.log(birthday)
+        console.log(location)
+
 
 
     }
@@ -36,19 +40,27 @@ function Registration()
                     </div>
 
                     <div className="input">                        
-                        <select  value={gender} onChange={(e) => setGender(e.target.value)} id='gender' name="gender">
-                            <option value="">Male</option>
+                        <select value={gender} onChange={(e) => setGender(e.target.value)} id='gender' name="gender">
+                            <option value="male">Male</option>
                             <option value="female">Female</option>
                         </select>               
-
                     </div>
 
-                    <div className="input">                 
-                    <input value={birthday} onChange={(e) => setBirthday(e.target.value)} type="datetime-local" id="birthdaytime" name="birthdaytime"></input>
+                    <div className='input-radio'>
+                        <select value={location} onChange={(e) => setLocation(e.target.value)} id='location' name="location">
+                            <option value="indian">Indian</option>
+                            <option value="nonIndian">Non-Indian</option>
+                        </select>
+                    </div>
+                 
+
+                    <div className="input">    
+                    <p>The time is in 24 HR Format</p> 
+                    <input value={birthday} onChange={(e) => setBirthday(e.target.value)} type="datetime-local" id="birthdaytime" required name="birthdaytime"></input>
                     </div>
 
                     <div className="input">
-                        <input value={birthPlace} onChange={(e) => setBirthPlace(e.target.value)} type="text" id="birthPlace" placeholder="Birth Place" name="birthPlace" />                        
+                        <input value={birthPlace} onChange={(e) => setBirthPlace(e.target.value)} type="search" id="birthPlace" placeholder="Birth Place" name="birthPlace" />                        
                         <FaUser className="icon" />
                     </div>
 
