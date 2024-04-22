8673886d016dfba7ea046ac8c01830090b58a861
tasmaishelke <tasmaishelke@gmail.com>
2024-04-22 11:18:52 +0530

    axios library installed, json fetching done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,38 @@
 import './about.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function About()
 {
-    return(
+    
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-        <div className="class-title-container">
-            <h1> About Page </h1>        
+  useEffect(() => {
+    axios.get('https://horoscope-4b2b3-default-rtdb.asia-southeast1.firebasedatabase.app/WorldLocations.json')
+      .then(response => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
 
-        </div>
-    );
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div>
+      <h1>Data Loaded</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+
+
 }
 
 export default About
